90dc82a47372c8cdb9aaf5ef6e30f6d1f8042f1f
220779 <102145019+220779@users.noreply.github.com>
2022-12-10 11:52:06 +0200

    [add] - read items from Database (JPA)

--- src/main/java/ee/projektPlus/quiz/StartupRunner.java
+++ src/main/java/ee/projektPlus/quiz/StartupRunner.java
@@ -2,11 +2,14 @@ package ee.projektPlus.quiz;
 
 import ee.projektPlus.quiz.database.entities.PlayerEntity;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import jakarta.transaction.Transactional;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Log
@@ -24,5 +27,13 @@ public class StartupRunner implements CommandLineRunner {
 
         entityManager.persist(player);
         log.info("Same player after persist: " + player);
+
+        log.info("List of player from Database :");
+        Query q = entityManager.createQuery("SELECT p FROM PLAYERS p");
+        List<PlayerEntity> playersFromDB = (List<PlayerEntity>)q.getResultList();
+
+        for (PlayerEntity playerFromDB : playersFromDB) {
+            log.info("Player from DB: " + playerFromDB);
+        }
     }
 }
